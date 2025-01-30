6c67c4c3a752c8373d1698f1e51f53d9d412865d
Khanak Agrawal <147262747+Khanakag-17@users.noreply.github.com>
2025-01-30 12:37:16 +0530

    Add files via upload

---
+++ music_library.py
@@ -0,0 +1,7 @@
+music = {
+    's': "https://www.youtube.com/watch?v=QYh6mYIJG2Y&pp=ygUHNyByaW5ncw%3D%3D",
+    'm': "https://www.youtube.com/watch?v=uuZE_IRwLNI&pp=ygUZbWlycm9ycyBqdXN0aW4gdGltYmVybGFrZQ%3D%3D",
+    'p': "https://www.youtube.com/watch?v=tcYodQoapMg&pp=ygUXcG9zaXRpb25zIGFyaWFuYSBncmFuZGU%3D",
+    'e': "https://www.youtube.com/watch?v=eVli-tstM5E&pp=ygUaZXNwcmVzc28gc2FicmluYSBjYXJwZW50ZXI%3D",
+    'a': "https://www.youtube.com/watch?v=nfs8NYg7yQM&pp=ygUWYXR0ZW50aW9uIGNoYXJsaWUgcHV0aA%3D%3D"
+}
